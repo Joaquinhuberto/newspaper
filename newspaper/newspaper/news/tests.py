1d40db88ae1408aa0a9ec05c78cae8e579d5ad49
Joaquín García <jgarcialanzas@gmail.com>
2018-07-01 00:41:44 +0200

    Clase 6: Creación del primer test

--- newspaper/newspaper/news/tests.py
+++ newspaper/newspaper/news/tests.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
 from django.test import TestCase
+from django.test.client import Client
 
-# Create your tests here.
+from newspaper.news.models import News
+
+
+class newsTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=False)
+
+    def test_newslist(self):
+        news = News.objects.create(title='Mi noticia test',
+                                   description='Mi descripcion test',
+                                   publish_date=datetime.now())
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+        self.assertIn(news.title, response.content)
+        self.assertIn(news.description, response.content)
